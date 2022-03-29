984ea0c08df3b5b3290638c5ee6497ef28e71bfe
jwl22 <jw.lee@yonsei.ac.kr>
2022-03-29 18:20:38 +0900

    Add PageControl_Traning

--- PageControl_Traning/PageControl_Traning/ViewController.swift
+++ PageControl_Traning/PageControl_Traning/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var lblNumber: UILabel!
+    @IBOutlet var pageControl: UIPageControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        pageControl.numberOfPages = 10
+        pageControl.currentPage = 0
+        pageControl.pageIndicatorTintColor = UIColor.gray
+        pageControl.currentPageIndicatorTintColor = UIColor.darkGray
     }
 
-
+    @IBAction func PageChange(_ sender: UIPageControl) {
+        lblNumber.text = String(pageControl.currentPage + 1)
+    }
+    
 }
 
