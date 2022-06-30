83b55a4c8c06e1214bcd949bb8b060411e465f63
jwl22 <jw.lee@yonsei.ac.kr>
2022-06-30 21:22:11 +0900

    Add login check

--- myproject/pybo/views/auth_views.py
+++ myproject/pybo/views/auth_views.py
@@ -1,3 +1,5 @@
+import functools
+
 from flask import Blueprint, url_for, render_template, flash, request, session, g
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import redirect
@@ -38,8 +40,11 @@ def login():
             error = "비밀번호가 올바르지 않습니다."
         if error is None:
             session.clear()
-            session['user_id'] = user.id
-            return redirect(url_for('main.index'))
+            _next = request.args.get('next', '')
+            if _next:
+                return redirect(_next)
+            else:
+                return redirect(url_for('main.index'))
         flash(error)
     return render_template('auth/login.html', form=form)
 
@@ -57,3 +62,13 @@ def load_logged_in_user():
 def logout():
     session.clear()
     return redirect(url_for('main.index'))
+
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(*args, **kwargs):
+        if g.user is None:
+            _next = request.url if request.method == 'GET' else ''
+            return redirect(url_for('auth.login', next=_next))
+        return view(*args, **kwargs)
+    return wrapped_view
